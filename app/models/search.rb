45ab16d1b9590c9ae4a0c465f65fd226481fa566
Doyi Lee <doyilee@hotmail.com>
2015-07-29 15:09:54 -0600

    search can find visual artist

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,25 +1,13 @@
 class Search < ActiveRecord::Base
 	def search_profile
-		profiles = Profile.all
-		profile = profiles.where(["name LIKE ? OR bio LIKE ?", "%#{keywords}%", "%#{keywords}%"]) if keywords.present?
+		profile=Profile.where(["name LIKE ? OR bio LIKE ?", "%#{keywords}%", "%#{keywords}%"]) if keywords.present?
 		
 		#finding musicians
 		profile = Profile.joins(:musician).where(:musicians => { :instrument => "#{instrument}" }) if instrument.present?
 		profile = Profile.joins(:musician).where(:musicians => { :genre => "#{genre}" }) if genre.present?
-
-		#find by address
-		profile = Profile.joins(:musician).where(:musicians => { :instrument => "#{instrument}" }) if instrument.present?
 		
-		musician = Musician.joins(:addresses).where(addresses: { :zip => "#{zip}" }) if zip.present?
-		Musician.includes(:addresses).references(:addresses).to_a
+		# finding visual artist
+		profile = Profile.joins(:visual_artist).where(:visual_artist => { :medium => "#{medium}"}) if medium.present?
 		return profile
-	end
-
-	def search_address
-		# musician = Musician.joins(:addresses).where(addresses: {"state LIKE ?", "#{state}"}) if zip.present?
-		# venue_rep = VenueRep.joins(:addresses).where(addresses: {"zip LIKE ?", "#{zip}"}) if zip.present?
-		# venue_rep = VenueRep.joins(:addresses).where(addresses: {"state LIKE ?", "#{state}"}) if zip.present?
-		# visual_artist = VisualArtist.joins(:addresses).where(addresses: {"zip LIKE ?", "#{zip}"}) if zip.present?
-		# visual_artist = VisualArtist.joins(:addresses).where(addresses: {"state LIKE ?", "#{state}"}) if zip.present?
 	end
 end 
