ef6f9da51f6dd5ed6f017cd48b62e1259a3c9ed3
Tiffany Forsyth <tiffanyforsyth1@gmail.com>
2015-07-29 21:38:48 -0600

    stying to visitor landing page, personal info, a bunch of testing

--- spec/controllers/photos_controller_spec.rb
+++ spec/controllers/photos_controller_spec.rb
@@ -2,12 +2,21 @@ require 'rails_helper'
 
 RSpec.describe PhotosController, type: :controller do
 
+let(:photo) {Photo.create(caption: 'My favorite shows')}
+
   # describe "GET #index" do
   #   it "returns http success" do
   #     get :index
   #     expect(response).to have_http_status(:success)
   #   end
   # end
+
+ describe "GET #show" do
+    it "returns http success" do
+      get :show, id: photo.id
+      expect(response).to have_http_status(:success)
+    end
+  end
 
   describe "GET #new" do
     it "returns http success" do
@@ -16,17 +25,24 @@ RSpec.describe PhotosController, type: :controller do
     end
   end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show, id: photo.id
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "POST #create" do
+  #   it "creates a new photo successfully" do
+  #     post :create, photo: {caption: 'Rob Zombie'}
+  #     expect(response).to have_http_status(:redirect)
+  #   end
+  # end
 
-  describe "GET #edit" do
-    it "returns http success" do
-      get :edit, id: photo.id
-      expect(response).to have_http_status(:success)
+  # describe "GET #edit" do
+  #   it "returns http success" do
+  #     get :edit, id: photo.id
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+  describe "DELETE #destroy" do
+    it "deletes photo successfully" do
+      delete :destroy, id: photo.id
+      expect(response).to have_http_status(:redirect)
     end
   end
 
